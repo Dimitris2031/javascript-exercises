5064a353e8ea1693322d22c0dac735e9a41fde81
kokranisd <dimitriskokranis21@gmail.com>
2023-12-03 01:03:59 +0000

    Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let convertTocelcius = ((Fahrenheit-32)*5)/9;
+  let roundCelcius = convertTocelcius.toFixed(1);
+  return Number(roundCelcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let convertTofahrenheit = ((Celsius*1.8)+32);
+
+  let roundFahrenheit = convertTofahrenheit.toFixed(1);
+  return Number(roundFahrenheit);
 };
 
 // Do not edit below this line
